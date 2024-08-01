92796e6aba8898cdbba1d9bb658838b97101f707
1yne <tnambiar2008@gmail.com>
2024-08-01 22:05:28 +0530

    Add UNSC and HRC logos

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
 export { default as Navbar } from "./components/Navbar.svelte"
+export { default as UNSCLogo } from "./components/UNSC.svelte"
+export { default as HRCLogo } from "./components/HRC.svelte"
