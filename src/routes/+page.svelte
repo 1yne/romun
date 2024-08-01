cd209860482de577d6c59111084d2bb8853a40e9
1yne <tnambiar2008@gmail.com>
2024-08-01 22:13:20 +0530

    Use dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,19 @@
-<div class="w-full flex items-center flex-col gap-4">
-  <h1 class="font-montserrat font-extrabold text-9xl text-white mt-24">RoMUN VIII</h1>
-  <h1 class="font-montserrat font-semibold text-4xl text-white">2024</h1>
+<script lang="ts">
+  import { Card } from "flowbite-svelte"
+  import { HRCLogo, UNSCLogo } from "$lib";
+</script>
+
+<div class="w-full">
+  <div class="w-full flex items-center flex-col gap-4">
+    <h1 class="font-montserrat font-extrabold text-9xl text-white mt-24">RoMUN VIII</h1>
+    <h1 class="font-montserrat font-semibold text-4xl text-white">2024</h1>
+  </div>
+
+  <div>
+    {#each [HRCLogo, UNSCLogo] as item}
+      <Card>
+        <HRCLogo width={200} fill="D60202" />
+      </Card>
+    {/each}
+  </div>
 </div>
