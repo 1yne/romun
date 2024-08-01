682b628dc02832ce9f6bde3dab7af973cec916f7
1yne <tnambiar2008@gmail.com>
2024-08-01 22:44:53 +0530

    Remove exporter file

--- src/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Navbar } from "./components/Navbar.svelte"
-export { default as UNSCLogo } from "./components/UNSC.svelte"
-export { default as HRCLogo } from "./components/HRC.svelte"
