a45e15c93383da20ae5896fa117f72bd2870bc44
1yne <tnambiar2008@gmail.com>
2024-08-12 22:45:45 +0530

    Adjust padding of school logo in mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,7 +56,7 @@
 </svelte:head>
 
 <div class="w-full font-montserrat">
-	<div class="absolute flex h-28 w-full px-12 py-4">
+	<div class="absolute flex h-28 w-full px-12 max-[645px]:px-6 transition-all py-4">
 		<img src="/RCISLogo.png" alt="RCIS Logo" class="w-24" />
 	</div>
 	<div class="heroSection flex min-h-screen items-center py-24">
