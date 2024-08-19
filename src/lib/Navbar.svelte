fda3bd47e5aa43cd230c8b768dde7306f3a01f8c
1yne <tnambiar2008@gmail.com>
2024-08-19 23:07:22 +0530

    Add error page

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,3 +1,3 @@
-<div class="w-full flex px-12 py-4 h-28">
-  <img src="/RCISLogo.png" alt="RCIS Logo" class="w-24">
+<div class="flex h-28 w-full px-12 py-4">
+	<a href="/"><img src="/RCISLogo.png" alt="RCIS Logo" class="w-24" /></a>
 </div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { page } from '$app/stores';
+	import ErrorLogo from '$lib/ErrorLogo.svelte';
+	import Navbar from '$lib/Navbar.svelte';
+	import { Button } from 'flowbite-svelte';
+</script>
+
+<Navbar />
+<div class="flex w-full flex-col items-center">
+	<ErrorLogo width={1000} />
+	<h1 class="mb-12 font-montserrat text-6xl font-bold uppercase text-white">
+		{$page.error?.message}
+	</h1>
+	<Button class="hover:!bg-purpleHover !bg-purple p-6 px-12 py-4 text-white transition-all" href="/"
+		>Return to home page</Button
+	>
+</div>
