413f8b8166fb81d0451859ec54a3d7553e68a731
1yne <tnambiar2008@gmail.com>
2024-08-21 18:56:59 +0530

    Remove transitions from navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,17 +1,3 @@
-<script lang="ts">
-	import { io } from '@svelteuidev/composables';
-	import { fade } from "svelte/transition"
-
-	let visible: boolean;
-	const handleChange = () => (visible = true);
-</script>
-
-<div
-	class="flex h-28 w-full px-12 py-4 transition-all max-[645px]:px-6"
-	use:io={{ threshold: 1 }}
-	on:change={handleChange}
->
-	{#if visible}
-		<a href="/" in:fade><img src="/RCISLogo.png" alt="RCIS Logo" class="w-24" /></a>
-	{/if}
+<div class="navbar flex h-28 w-full px-12 py-4 transition-all max-[645px]:px-6">
+	<a href="/"><img src="/RCISLogo.png" alt="RCIS Logo" class="w-24" /></a>
 </div>
