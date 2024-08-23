ec55dec73fe9aed0f077eb72e2bb2572a668c7a6
1yne <tnambiar2008@gmail.com>
2024-08-23 21:27:02 +0530

    Clarify /register route

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,3 +1,4 @@
 <div class="p-12">
-  <a href="/" class="text-white">Back</a>
+  <h1 class="text-white mb-8">This is just a page made for testing navigation. This page will be deleted in the official website, and the "Register" button will lead to a link</h1>
+  <a href="/" class="text-white bg-purple hover:bg-purpleHover transition-all p-4 rounded-md px-8">Back</a>
 </div>
