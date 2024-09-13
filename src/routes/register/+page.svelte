1d3005f88a54de6a16804a8a19d4bf7e6fb4bc20
1yne <tnambiar2008@gmail.com>
2024-09-14 00:16:06 +0530

    Use history.back() to navigate back in /register

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,9 +1,12 @@
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div class="p-12">
 	<h1 class="mb-8 text-white">
 		This is just a page made for testing navigation. This page will be deleted in the official
 		website, and the "Register" button will lead to a link
 	</h1>
-	<a href="/" class="rounded-md bg-purple p-4 px-8 text-white transition-all hover:bg-purpleHover"
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-missing-attribute -->
+	<a on:click={() => history.back()} class="rounded-md bg-purple p-4 px-8 text-white transition-all hover:bg-purpleHover"
 		>Back</a
 	>
 </div>
