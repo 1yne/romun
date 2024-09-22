3ec9badb0a5d67b857848a4f60dac44fa5fcb3d4
1yne <tnambiar2008@gmail.com>
2024-09-22 22:54:50 +0530

    Remove transparent black bg on navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,5 +1,5 @@
 <div
-	class="navbar fixed top-0 h-24 z-[1000] flex w-full bg-black/50 p-4 px-8 transition-all max-[645px]:px-6"
+	class="navbar fixed top-0 h-24 z-[1000] flex w-full p-4 px-8 transition-all max-[645px]:px-6"
 >
 	<a href="/"><img src="/RCISLogo.png" alt="RCIS Logo" class="w-20" /></a>
 </div>
