7e7906465a287848cd4547126a1f2c34f90d3dfe
1yne <tnambiar2008@gmail.com>
2024-09-23 17:17:46 +0530

    Add back arrow in pages other than /

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,7 +1,31 @@
+<script lang="ts">
+	import ArrowLeft from 'carbon-icons-svelte/lib/ArrowLeft.svelte';
+	import { page } from '$app/stores';
+	import { fly } from 'svelte/transition';
+</script>
+
 <div
-	class="navbar fixed top-0 h-24 z-[1000] flex w-full p-4 px-8 transition-all max-[645px]:px-6"
+	class="navbar fixed top-0 z-[1000] flex h-24 w-full p-4 px-8 text-white transition-all max-[645px]:px-6"
 >
-	<a href="/"><img src="/RCISLogo.png" alt="RCIS Logo" class="w-20" /></a>
+	<a href="/">
+		{#if $page.route.id === '/'}
+			<img
+				src="/RCISLogo.png"
+				alt="RCIS Logo"
+				class="w-20"
+				in:fly|global={{ y: -10, duration: 500, delay: 700 }}
+				out:fly|global={{ y: -10, duration: 500 }}
+			/>
+		{:else}
+			<div
+				in:fly|global={{ y: 10, duration: 500, delay: 700 }}
+				out:fly|global={{ y: 10, duration: 500 }}
+				class="cursor-pointer"
+			>
+				<ArrowLeft size={32} />
+			</div>
+		{/if}
+	</a>
 </div>
 
 <style>
