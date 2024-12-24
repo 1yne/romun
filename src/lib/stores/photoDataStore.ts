9664c73285539171907c53f2a11f84c441d5a622
1yne <tnambiar2008@gmail.com>
2024-12-24 15:29:41 +0530

    Add photos to gallery

---
+++ src/lib/stores/photoDataStore.ts
@@ -0,0 +1,243 @@
+import { readable } from 'svelte/store';
+
+const data = [
+    {
+      src: '/2024Photos/1.jpg',
+    },
+    {
+      src: '/2024Photos/2.jpg',
+    },
+    {
+      src: '/2024Photos/3.jpg',
+    },
+    {
+      src: '/2024Photos/4.jpg',
+    },
+    {
+      src: '/2024Photos/5.jpg',
+    },
+    {
+      src: '/2024Photos/6.jpg',
+    },
+    {
+	  src: '/2024Photos/7.jpg',
+    },
+    {
+      src: '/2024Photos/8.jpg',
+    },
+	{
+      src: '/2024Photos/9.jpg',
+    },
+    {
+      src: '/2024Photos/10.jpg',
+    },
+    {
+      src: '/2024Photos/11.jpg',
+    },
+    {
+      src: '/2024Photos/12.jpg',
+    },
+    {
+      src: '/2024Photos/13.jpg',
+    },
+    {
+      src: '/2024Photos/14.jpg',
+    },
+    {
+	  src: '/2024Photos/15.jpg',
+    },
+    {
+      src: '/2024Photos/16.jpg',
+    },
+	{
+      src: '/2024Photos/17.jpg',
+    },
+    {
+      src: '/2024Photos/18.jpg',
+    },
+    {
+      src: '/2024Photos/19.jpg',
+    },
+    {
+      src: '/2024Photos/20.jpg',
+    },
+    {
+      src: '/2024Photos/21.jpg',
+    },
+    {
+      src: '/2024Photos/22.jpg',
+    },
+    {
+	  src: '/2024Photos/23.jpg',
+    },
+    {
+      src: '/2024Photos/24.jpg',
+    },
+	{
+      src: '/2024Photos/25.jpg',
+    },
+    {
+      src: '/2024Photos/26.jpg',
+    },
+    {
+      src: '/2024Photos/27.jpg',
+    },
+    {
+      src: '/2024Photos/28.jpg',
+    },
+    {
+      src: '/2024Photos/29.jpg',
+    },
+    {
+      src: '/2024Photos/30.jpg',
+    },
+    {
+	  src: '/2024Photos/31.jpg',
+    },
+    {
+      src: '/2024Photos/32.jpg',
+    },
+	{
+      src: '/2024Photos/33.jpg',
+    },
+    {
+      src: '/2024Photos/34.jpg',
+    },
+    {
+      src: '/2024Photos/35.jpg',
+    },
+    {
+      src: '/2024Photos/36.jpg',
+    },
+    {
+      src: '/2024Photos/37.jpg',
+    },
+    {
+      src: '/2024Photos/38.jpg',
+    },
+    {
+	  src: '/2024Photos/39.jpg',
+    },
+    {
+      src: '/2024Photos/40.jpg',
+    },
+	{
+      src: '/2024Photos/41.jpg',
+    },
+    {
+      src: '/2024Photos/42.jpg',
+    },
+    {
+      src: '/2024Photos/43.jpg',
+    },
+    {
+      src: '/2024Photos/44.jpg',
+    },
+    {
+      src: '/2024Photos/45.jpg',
+    },
+    {
+      src: '/2024Photos/46.jpg',
+    },
+    {
+	  src: '/2024Photos/47.jpg',
+    },
+    {
+      src: '/2024Photos/48.jpg',
+    },
+	{
+      src: '/2024Photos/49.jpg',
+    },
+    {
+      src: '/2024Photos/50.jpg',
+    },
+    {
+      src: '/2024Photos/51.jpg',
+    },
+    {
+      src: '/2024Photos/52.jpg',
+    },
+    {
+      src: '/2024Photos/53.jpg',
+    },
+    {
+      src: '/2024Photos/54.jpg',
+    },
+    {
+	  src: '/2024Photos/55.jpg',
+    },
+    {
+      src: '/2024Photos/56.jpg',
+    },
+	{
+      src: '/2024Photos/57.jpg',
+    },
+    {
+      src: '/2024Photos/58.jpg',
+    },
+    {
+      src: '/2024Photos/59.jpg',
+    },
+    {
+      src: '/2024Photos/60.jpg',
+    },
+    {
+      src: '/2024Photos/61.jpg',
+    },
+    {
+      src: '/2024Photos/62.jpg',
+    },
+    {
+	  src: '/2024Photos/63.jpg',
+    },
+    {
+      src: '/2024Photos/64.jpg',
+    },
+	{
+	  src: '/2024Photos/65.jpg',
+    },
+	{
+	  src: '/2024Photos/66.jpg',
+    },
+	{
+	  src: '/2024Photos/67.jpg',
+    },
+	{
+	  src: '/2024Photos/68.jpg',
+    },
+	{
+	  src: '/2024Photos/69.jpg',
+    },
+	{
+	  src: '/2024Photos/70.jpg',
+    },
+	{
+	  src: '/2024Photos/71.jpg',
+    },
+	{
+	  src: '/2024Photos/72.jpg',
+    },
+	{
+	  src: '/2024Photos/73.jpg',
+    },
+	{
+	  src: '/2024Photos/74.jpg',
+    },
+	{
+	  src: '/2024Photos/75.jpg',
+    },
+	{
+	  src: '/2024Photos/76.jpg',
+    },
+	{
+	  src: '/2024Photos/77.jpg',
+    },
+	{
+	  src: '/2024Photos/78.jpg',
+    },
+	{
+	  src: '/2024Photos/57.jpg',
+    },
+  ];;
+
+export const photoDataStore = readable(data);
