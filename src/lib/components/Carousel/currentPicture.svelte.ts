8ccc905bb09573f0d7e813e3ac0c9f639c264a96
1yne <tnambiar2008@gmail.com>
2025-09-13 17:35:47 +0530

    Update agendas

--- src/lib/components/Carousel/currentPicture.svelte.ts
+++ src/lib/components/Carousel/currentPicture.svelte.ts
@@ -53,7 +53,7 @@ export const allDataStore = writable([
 		name: 'HRC',
 		full: 'Human Rights Council',
 		link: '/committees/hrc',
-		agenda: '',
+		agenda: `Addressing the Recruitment and Use of Child Soldiers in Armed Conflicts`,
 		description: `In today's world, many suffer as fundamental human, social, and cultural rights are relinquished. The UNHRC stands as a global advocate, defending these rights and promoting justice and dignity, especially in crises like the Rohingya, weaving understanding into hope for a more equitable world.`
 	},
 	{
@@ -61,7 +61,7 @@ export const allDataStore = writable([
 		name: 'UNSC',
 		full: 'United Nations Security Council',
 		link: '/committees/unsc',
-		agenda: '',
+		agenda: 'Deliberation on the Escalating Conflict in Sudan',
 		description: `The Security Council of the United Nations maintains international peace and security through sanctions, authorized force, peacekeeping missions, and non-proliferation efforts. It investigates disputes, recommends settlements, and ensures compliance with its binding resolutions, upholding international law and contributing to global stability.`
 	},
 	{
@@ -69,7 +69,7 @@ export const allDataStore = writable([
 		name: 'AIPPM',
 		full: 'All India Political Party Meet',
 		link: '/committees/aippm',
-		agenda: '',
+		agenda: `Safeguarding India's National Security: Addressing Cross-Border Terrorism and Strategic Threats in Light of Recent Escalations`,
 		description: `In Bharat, the All-India Political Parties Meet addresses critical issues. This forum fosters national dialogue, promoting transparency, accountability, and the rule of law to preserve our democratic integrity and ensure a fair electoral process for future generations.`
 	},
 	{
@@ -77,7 +77,7 @@ export const allDataStore = writable([
 		name: 'CCC',
 		full: 'Continous Crisis Committee',
 		link: '/committees/ccc',
-		agenda: '',
+		agenda: `Strategic Response to the Discovery of Soviet Missile Installations in Cuba (Freeze Date: 16 October 1962`,
 		description: `This committee thrives on the intensity of constantly evolving scenarios, requiring quick thinking, adaptability, and strategic problem-solving. Delegates must stay on their toes, as crises can escalate or shift direction at any moment, pushing participants to showcase their diplomatic agility and collaboration skills. It's a high-octane experience, perfect for those who relish a challenge and the unpredictability of real-world events.`
 	},
 	{
@@ -85,7 +85,7 @@ export const allDataStore = writable([
 		name: 'DISEC',
 		full: 'Disarmament and International Security',
 		link: '/committees/disec',
-		agenda: '',
+		agenda: `Reassessing the Global Non-Proliferation Architecture in Light of Iran's Renewed Enrichment Activities`,
 		description: `DISEC addresses the global security threats of LAWs and UAVs, balancing strategic advantages with ethical oversight. Delegates will negotiate frameworks to regulate these technologies, ensuring international stability and compliance with humanitarian law.`
 	}
 ]);
