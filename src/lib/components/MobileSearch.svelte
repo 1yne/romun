d326f27500bcd2fe41e8f7de551adf45c6874f33
1yne <tnambiar2008@gmail.com>
2025-09-15 18:29:01 +0530

    Collapse mobile search after clicking on link

--- src/lib/components/MobileSearch.svelte
+++ src/lib/components/MobileSearch.svelte
@@ -6,9 +6,7 @@
 	import { clickOutside } from 'svelte-outside';
 	import { store } from '$lib/stores/store';
 
-	let time = $state(0);
-
-	$inspect($store.searchHover);
+	let time = $state(new Date().getTime());
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
@@ -17,19 +15,25 @@
 	<button
 		class={`flex ${$store.searchHover ? 'h-48 w-[calc(100vw-4rem)] px-6' : 'h-12 w-12'} items-left flex-col justify-center gap-4 rounded-3xl border border-white/50 bg-black/75 py-4 transition-all hover:border-white`}
 		onclick={() => {
-			$store.searchHover = true;
-			time = new Date().getTime()
+			if ((new Date().getTime() - time) / 1000 > 0.05) {
+				$store.searchHover = true;
+				time = new Date().getTime();
+			}
 		}}
 		use:clickOutside={() => {
 			if ((new Date().getTime() - time) / 1000 > 0.05) {
-				$store.searchHover = false
-			};
+				$store.searchHover = false;
+			}
 		}}
 	>
 		{#if $store.searchHover}
 			<a
 				href="/committees"
 				in:fly={{ y: 40, duration: 750, easing: backInOut }}
+				onclick={() => {
+					time = new Date().getTime();
+					$store.searchHover = false;
+				}}
 				class={`font-montserrat uppercase ${$page.route.id?.includes('committees') ? 'text-white' : 'text-white/50'} text-left transition-all hover:text-white`}
 				>Committees</a
 			>
@@ -37,6 +41,10 @@
 			<a
 				href="/register"
 				in:fly={{ y: 40, duration: 750, delay: 100, easing: backInOut }}
+				onclick={() => {
+					time = new Date().getTime();
+					$store.searchHover = false;
+				}}
 				class="text-left font-montserrat uppercase text-white/50 transition-all hover:text-white"
 				>Register</a
 			>
@@ -44,6 +52,10 @@
 			<a
 				href="https://docs.google.com/spreadsheets/d/1GkEe7XvZHeQqnCivft9qSg0pZhzDClgj2deLxR6LEbk/edit?usp=sharing"
 				in:fly={{ y: 40, duration: 750, delay: 100, easing: backInOut }}
+				onclick={() => {
+					time = new Date().getTime();
+					$store.searchHover = false;
+				}}
 				class="text-left font-montserrat uppercase text-white/50 transition-all hover:text-white"
 				>Country Matrix</a
 			>
